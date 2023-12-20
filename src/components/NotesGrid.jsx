34fa977161612e4a3a3a597c9eb761987eb718ac
Ahmad Shaker <theahmadshaker@gmail.com>
2023-12-20 21:31:01 +0200

    Progress on the UI and animations on the sidebar

--- src/components/NotesGrid.jsx
+++ src/components/NotesGrid.jsx
@@ -3,8 +3,11 @@ import NotesCard from "./NotesCard";
 
 const NotesGrid = () => {
   return (
-    <div className="flex flex-col items-start justify-start flex-grow h-full">
+    <div className="flex flex-col items-start justify-start flex-grow h-full w-full">
       <Searchbar />
+      <div className="flex flex-row items-center justify-start px-12 py-6">
+        <h1 className="text-4xl text-black font-semibold">Notes</h1>
+      </div>
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-10 w-full p-4 justify-center overflow-y-auto">
         <NotesCard />
         <NotesCard />
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -4,14 +4,24 @@ import { useState } from "react";
 // HeadlessUI
 import { Disclosure, RadioGroup } from "@headlessui/react";
 
+// Framer
+import { motion, AnimatePresence } from "framer-motion";
+
 const Sidebar = () => {
-  const [colors, setColors] = useState([]);
+  const colorClassnames = [
+    "#7c3aed",
+    "#2563eb",
+    "#059669",
+    "#eab308",
+    "#e11d48",
+    "#db2777",
+  ];
 
   return (
-    <div className="h-full w-1/12 flex flex-col items-center justify-start border-r border-gray-200 py-10 space-y-4">
-      <h1 className="text-sm font-semibold">Noterize</h1>
+    <div className="h-full w-1/12 flex flex-col items-center justify-start border-r border-gray-200 py-10 space-y-8">
+      <h1 className="text-xl font-semibold">Noterize</h1>
       <div className="flex  flex-col items-center justify-start">
-        <CategorySelector colors={colors} />
+        <CategorySelector colors={colorClassnames} />
       </div>
     </div>
   );
@@ -20,7 +30,14 @@ const Sidebar = () => {
 export default Sidebar;
 
 const CategorySelector = ({ colors }) => {
-  const [activeColor, setActiveColor] = useState(null);
+  const [activeColor, setActiveColor] = useState("All");
+
+  const animationDelay = 0.1; // delay between each bubble animation in seconds
+
+  // This function sets the color for the last bubble which will be a filter that includes all notes
+  const generateGradient = (colors) => {
+    return `linear-gradient(45deg, ${colors.join(", ")})`;
+  };
 
   return (
     <Disclosure>
@@ -44,46 +61,56 @@ const CategorySelector = ({ colors }) => {
               />
             </svg>
           </Disclosure.Button>
-          <Disclosure.Panel>
-            <RadioGroup
-              className="flex flex-col items-center justify-center py-6 space-y-6"
-              value={activeColor}
-            >
-              {colors.map((color) => {
-                <ColorBubble backgroundColor={color} />;
-              })}
-            </RadioGroup>
-            <CategoryCreator />
-          </Disclosure.Panel>
+
+          {open && (
+            <Disclosure.Panel static>
+              <RadioGroup
+                className="flex flex-col items-center justify-center py-6 space-y-[32px]"
+                value={activeColor}
+              >
+                {colors.map((color, index) => (
+                  <ColorBubble
+                    key={color}
+                    color={color}
+                    delay={index * animationDelay}
+                    setActiveColor={() => {
+                      setActiveColor(color), console.log(activeColor);
+                    }}
+                  />
+                ))}
+                <ColorBubble
+                  key="gradient"
+                  color={generateGradient(colors)}
+                  delay={colors.length * animationDelay}
+                  setActiveColor={() => setActiveColor("All")}
+                />
+              </RadioGroup>
+            </Disclosure.Panel>
+          )}
         </>
       )}
     </Disclosure>
   );
 };
 
-const ColorBubble = ({ color }) => {
-  return (
-    <button
-      className="w-6 h-6  rounded-full"
-      style={{ backgroundColor: color }}
-    />
-  );
-};
+const ColorBubble = ({ color, delay, activeColor, setActiveColor }) => {
+  const initialY = -32; // Initial Y position offset
+  const isSelected = activeColor === color;
 
-const CategoryCreator = () => {
+  console.log("ColorBubble color:", color);
+
   return (
-    <button className="bg-gray-200 rounded-full w-6 h-6 flex flex-row items-center justify-center hover:brightness-105 duration-200">
-      <svg
-        viewBox="0 0 24 24"
-        fill="none"
-        xmlns="http://www.w3.org/2000/svg"
-        className="h-4 w-4"
-      >
-        <path
-          d="M19 11H13V5C13 4.73478 12.8946 4.48043 12.7071 4.29289C12.5196 4.10536 12.2652 4 12 4C11.7348 4 11.4804 4.10536 11.2929 4.29289C11.1054 4.48043 11 4.73478 11 5V11H5C4.73478 11 4.48043 11.1054 4.29289 11.2929C4.10536 11.4804 4 11.7348 4 12C4 12.2652 4.10536 12.5196 4.29289 12.7071C4.48043 12.8946 4.73478 13 5 13H11V19C11 19.2652 11.1054 19.5196 11.2929 19.7071C11.4804 19.8946 11.7348 20 12 20C12.2652 20 12.5196 19.8946 12.7071 19.7071C12.8946 19.5196 13 19.2652 13 19V13H19C19.2652 13 19.5196 12.8946 19.7071 12.7071C19.8946 12.5196 20 12.2652 20 12C20 11.7348 19.8946 11.4804 19.7071 11.2929C19.5196 11.1054 19.2652 11 19 11Z"
-          className="fill-white"
-        />
-      </svg>
-    </button>
+    <AnimatePresence>
+      <motion.button
+        initial={{ opacity: 0, y: initialY }}
+        animate={{ opacity: 1, y: 0, transition: { delay: delay } }}
+        exit={{ opacity: 0, y: initialY, transition: { duration: 0.2 } }}
+        className={`w-6 h-6 rounded-full duration-300 ${
+          isSelected ? `ring-2 ring-offset-2 ring-white` : ""
+        }`}
+        style={{ backgroundColor: color }}
+        onClick={setActiveColor}
+      />
+    </AnimatePresence>
   );
 };
